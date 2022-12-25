fb4b69a29063a39ed3b072785170c61282b569d8
Ruslan Lopez Carro <scherzo_16@hotmail.com>
2022-12-25 16:09:25 -0600

    separated things into method for quiz driver

--- src/quiz/QuizDriver.java
+++ src/quiz/QuizDriver.java
@@ -27,7 +27,31 @@ public final class QuizDriver {
      * @param args program arguments ( actually they are not parsed )
      */
     public static void main(final String... args) {
-        final Quiz doom = new Quiz();
+        final Quiz myQuiz = new Quiz();
+        loadQuestions(myQuiz);
+        myQuiz.askForCategories();
+        myQuiz.askForDifficulty();
+        //Display questions to user, receive and check response
+        myQuiz.askForSubsetSize();
+        myQuiz.displayAndCheckQuestions();
+        myQuiz.summarizeResults();
+        myQuiz.showFailed();
+    }
+
+    private static void loadQuestions(Quiz myQuiz) {
+        String[] dir = detectFilenamesInResourcesFolder();
+        Arrays.stream(dir).sequential().map(File::new).filter(file -> {
+            String fileName=file.getName();
+            String extension = fileName.substring(fileName.lastIndexOf('.'));
+            return ".txt".equalsIgnoreCase(extension);
+        }).forEach(txtFile->
+//                myQuiz.createQuestionsFromFile("quiz/resources/SampleQuiz.txt", true)
+                myQuiz.createQuestionsFromFile("quiz/resources/"+txtFile.getName(), true)
+
+        );
+    }
+
+    private static String[] detectFilenamesInResourcesFolder() {
         URL pathUrl = QuizDriver.class.getClassLoader().getResource("quiz/resources/");
         String[] dir = new String[0];
         if ((pathUrl != null) && pathUrl.getProtocol().equals("file")) {
@@ -37,22 +61,7 @@ public final class QuizDriver {
                 e.printStackTrace();
             }
         }
-        Arrays.stream(dir).sequential().map(File::new).filter(file -> {
-            String fileName=file.getName();
-            String extension = fileName.substring(fileName.lastIndexOf('.'));
-            return ".txt".equalsIgnoreCase(extension);
-        }).forEach(txtFile->
-                doom.createQuestionsFromFile("quiz/resources/"+txtFile.getName(), true)
-
-        );
-        //doom.createQuestionsFromFile("quiz/resources/SampleQuiz.txt", true);
-        doom.askForCategories();
-        doom.askForDifficulty();
-        //Display questions to user, receive and check response
-        doom.askForSubsetSize();
-        doom.displayAndCheckQuestions();
-        doom.summarizeResults();
-        doom.showFailed();
+        return dir;
     }
 
 }
